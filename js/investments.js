8507b5a0362f0bf5a7fbd3cfeb0cddae49230867
veekthooor360 <nwoguuchenna88@gmail.com>
2020-06-25 00:57:39 +0100

    message

--- js/investments.js
+++ js/investments.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
+
 $('#view-details').click(() => {
-=======
-$('.view-details').click(() => {
->>>>>>> 60496ea2cdfd798b2e5769ac765f44d786217eb4
+
+
     $("#investmentModal").modal("show");
 });
